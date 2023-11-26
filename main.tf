699eb72a0592caa2d8dc398a67d8e148df6fc0d6
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2023-11-26 14:41:27 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,3 +44,18 @@ resource "local_file" "tf-key" {
   filename = "tf-key-pair-${random_id.server.hex}"
 }
 
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1c"
+  size              = 20
+
+  tags = {
+    Name = "ec2 volume"
+  }
+}
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
+
+
